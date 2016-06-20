c7b4e48ea43ee5d4db45910c66abc923beefd5e7
Jemiah Sius <jae@theaddicts.co>
2016-06-19 20:56:02 -0400

    created event50 solution in xamarin

---
+++ Events50/Event.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace Events50
+{
+	public class Event
+	{
+		public string name;
+		public DateTime date;
+		public string speaker;
+		public string details;
+		public bool isPassed;
+		public int count;
+		public int totalCount;
+
+		public Event(name, date, details, isPassed)
+		{
+			this.name = name;
+			this.date = date;
+			this.details = details;
+			this.isPassed = isPassed;
+
+		}
+
+		public Event(name, date, speaker, details, isPassed)
+		{
+			this.name = name;
+			this.date = date;
+			this.speaker = speaker;
+			this.details = details;
+			this.isPassed = isPassed;
+
+		}
+	}
+}
+
